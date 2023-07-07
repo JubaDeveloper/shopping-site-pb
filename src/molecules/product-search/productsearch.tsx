c7c2dc83506fc35f9d657a808972261a364bfe6f
JubaDEV <michaelres3344@gmail.com>
2023-07-07 20:22:30 -0300

    added the input interaction in search mol

--- src/molecules/product-search/productsearch.tsx
+++ src/molecules/product-search/productsearch.tsx
@@ -1,9 +1,23 @@
-import { ReactElement } from "react";
+import { ChangeEvent, ReactElement } from "react";
 
-export const ProductSearch = (): ReactElement => {
+interface IProductSearchProps {
+    onSearchChange: (text: string) => never
+    type: React.HTMLInputTypeAttribute
+}
+
+export const ProductSearch = ({
+    onSearchChange,
+    type = "text" // Default
+}: IProductSearchProps): ReactElement => {
+    
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { target: { value } } = e
+        onSearchChange(value)
+    }
+
     return (
         <div className="flex gap-10 flex-row px-2">
-            <input placeholder="Type the product name" className="focus:outline-none appearance-none placeholder:px-2 text-lg min-h-[45px] min-w-[390px] rounded-lg border-2 border-black"/>
+            <input type={type} onChange={handleSearchChange} placeholder="Type the product name" className="focus:outline-none appearance-none placeholder:px-2 text-lg min-h-[45px] min-w-[390px] rounded-lg border-2 border-black"/>
             <button className="bg-black text-white font-rbs tracking-wide text-lg px-[30px] rounded-[10px]">
                 Search
             </button>
